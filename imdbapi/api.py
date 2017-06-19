f2750799870a879c3da6fbe299fb3d3fd1d10330
Neil Aitken <neil@skywire.co.uk>
2017-06-19 14:24:51 +0100

    Add beaker caching to imdbapi, add gitignore

--- imdbapi/api.py
+++ imdbapi/api.py
@@ -2,20 +2,26 @@ import urllib3
 from urllib import parse
 import json
 
-## http://www.theimdbapi.org/#
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+cache_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': './var/cache/data',
+    'cache.lock_dir': './var/cache/lock'
+}
+cache = CacheManager(**parse_cache_config_options(cache_opts))
 
 api_url = 'http://www.theimdbapi.org/api/'
-
-def search_titles(title, year=None):
+@cache.cache('search_titles', expire=3600)
+def search_titles(title):
     request = urllib3.PoolManager()
     params = {'title': title}
-    if (year):
-        params['year'] = year
     response = request.request('GET', api_url + 'find/movie?{}'.format(parse.urlencode(params)))
     data = response.data.decode('UTF-8')
     return json.loads(data)
 
-
+@cache.cache('get_matching_cast', expire=3600)
 def get_matching_cast(first_movie, second_movie):
     matching_cast = [(first, second)
                      for first in first_movie['cast']
@@ -23,6 +29,7 @@ def get_matching_cast(first_movie, second_movie):
                      if first['name'] == second['name']]
     return matching_cast
 
+@cache.cache('get_title', expire=3600)
 def get_title(id):
     request = urllib3.PoolManager()
     response = request.request('GET', api_url + 'movie?movie_id=' + id)
